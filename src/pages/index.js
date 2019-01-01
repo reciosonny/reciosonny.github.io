6daf7a04f7e1b3061261fc04fbd53f50743e80ed
Sonny R. Recio <reciosonny@gmail.com>
2019-01-01 21:03:30 +0800

    Restored old source files back after `npm run deploy`. DO NOT RUN THIS SCRIPT AGAIN

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+
+const IndexPage = () => (
+  <div>
+      <h1>Personal Website of Sonny Recio</h1>
+      <h2>Hello world!</h2>
+  </div>
+);
+
+export default IndexPage
