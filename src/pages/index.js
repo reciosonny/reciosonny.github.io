e8f0340d149fdb065d4a5fb1d379b95ae5a70bc7
Sonny R. Recio <reciosonny@gmail.com>
2019-01-01 18:14:04 +0800

    Small modifications and copy-pasted public folder files into root folder.

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,8 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
+    <h1>Personal Website of Sonny Recio</h1>
+    
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
